fe41c305908b583075d829a6f152ccd2f91f1358
LeeYinSong <Yinsonglee00@gmail.com>
2021-08-26 22:12:28 +0800

    .

---
+++ resources/views/MainPage.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.HomeNav')
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+
+
+
+    <div class="p-5 container-fluid">
+        <div style="text-align: right;">
+        <strong style="font-family:verdana;  position: relative;" >
+            <h1>Find out more job</h1>
+            </strong>
+            <p>Find the jobs now!</p>
+            <p>There are already about 20 companies settled in.</p>
+            <p>All the company that posted the recruitment notice in our website was credible.</p>
+            <p>Search your interest company or the most suitable company to make your life more colorful.</p>
+            <p>discovery key talent resources to get you ready for the job market and beyond.</p>
+        
+        </div>
+
+        <a href="{{ url('employer/EmployerLogin') }}" class="button"><button class="btn btn-danger" style="border-radius: 20em;" type="button" onclick="{{ url('user.Studentlogin') }}">Find More Job</button></a>
+    </div>
+
+    <div class="p-5 container-fluid" style="background: rgba(192,192,192, 0.4);">
+    <strong style="font-family:verdana;  position: relative;">
+        <h1>More talented Students here!</h1>
+        </strong>
+        <p>Find out out more talented students here!</p>
+            <p>Our job seekers are from different universitie.</p>
+            <p>Here all the student wants to learn and get more working experience.</p>
+            <p>The students at here are come form different field which is computer science, marketing, or design.</p>
+            <p>At here you can find more the principal students!</p>
+        
+            <a href="{{ url('user/Studentlogin') }}" class="button"><button class="btn btn-danger float-right" style="border-radius: 20em;" type="button">See More Resume</button></a>
+    </div>
+
+@endsection
--- resources/views/layouts/HomeNav.blade.php
+++ resources/views/layouts/HomeNav.blade.php
@@ -45,13 +45,12 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
-                        @guest
+                        <!-- @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('user.login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
-
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('user.register') }}">{{ __('Register') }}</a>
@@ -72,20 +71,18 @@
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::guard('web')->user()->name }}
                                 </a>
-
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
                                 </div>
                             </li>
-                        @endguest
+                        @endguest -->
                     </ul>
                 </div>
             </div>
